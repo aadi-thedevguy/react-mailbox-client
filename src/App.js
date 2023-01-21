5274ebcbdf9e7e8ca464478d0026e719ca00b01f
aadi-thedeyguy <adikhare24@gmail.com>
2023-01-21 13:32:08 +0530

    added login

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import { Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
+import Login from './components/Login';
 import Signup from './components/Signup';
+import Home from './components/Home'
 
 function App() {
 
   return (
     <div className="App" >
-      <Signup />
       
+      <Routes>
+          <Route path="/" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
     </div>
   )
 }
